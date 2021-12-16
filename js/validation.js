2a936741780441fe7f59275229f53cbf8f101ddc
Yuliya Sidarenka <84760360+YuliyaSi@users.noreply.github.com>
2021-12-16 01:04:36 +0100

    Add files via upload

---
+++ js/validation.js
@@ -0,0 +1,19 @@
+let validation = function () {
+    'use strict'
+
+    let forms = document.querySelectorAll('.needs-validation')
+
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  };
+  validation();
+
